f22bfe3952b07338890c0c1e73fa75e911211c1a
Gabriella Guimaraes <gabigara13@gmail.com>
2024-02-13 20:58:33 -0300

    feat: nav bar + responsividade

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,5 +1,3 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
-
-//Aqui serão configuradas as rotas da aplicação
