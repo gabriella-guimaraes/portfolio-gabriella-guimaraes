8fba6ffe914929b3d9db8232e9ee32f4bf1b4725
Gabriella Guimaraes <gabigara13@gmail.com>
2024-02-17 23:03:56 -0300

    feat: about page

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
