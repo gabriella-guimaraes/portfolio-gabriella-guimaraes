cea3ad5498be50b63c01dc20202e4c6e57637deb
Gabriella Guimaraes <gabigara13@gmail.com>
2024-02-20 12:55:29 -0300

    feat: Timeline component

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent, SkillsComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent, SkillsComponent, TimelineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
