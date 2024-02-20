c1e085a35be6994ff0edd9f049cca4796ec9d0b4
Gabriella Guimaraes <gabigara13@gmail.com>
2024-02-20 17:46:57 -0300

    feat: Education component + refactoring

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { TimelineComponent } from './components/timeline/timeline.component';
+import { EducationComponent } from './components/education/education.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent, SkillsComponent, TimelineComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent, SkillsComponent, TimelineComponent, EducationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
