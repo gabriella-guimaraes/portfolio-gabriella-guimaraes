5ba221490b35b0c3b7c2c68d12c4b5d5c7890d27
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-04 16:11:58 -0300

    feat: leftFloatAnimation and rightFloatAnimation

--- portfolio/src/app/utils/animations.ts
+++ portfolio/src/app/utils/animations.ts
@@ -13,3 +13,29 @@ export const floatAnimation = trigger('float', [
         animate('700ms ease-in')
     ])
 ]);
+
+export const rightFloatAnimation = trigger('right-float', [
+    state('void', style ({
+        transform: 'translateX(-50px)',
+        opacity:0
+    })),
+    transition('void => *', [
+        animate('700ms ease-out')
+    ]),
+    transition('* => void', [
+        animate('700ms ease-in')
+    ])
+]);
+
+export const leftFloatAnimation = trigger('left-float', [
+    state('void', style ({
+        transform: 'translateX(50px)',
+        opacity:0
+    })),
+    transition('void => *', [
+        animate('700ms ease-out')
+    ]),
+    transition('* => void', [
+        animate('700ms ease-in')
+    ])
+]);
