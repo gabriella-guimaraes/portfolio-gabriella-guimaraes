dcdd6f90f22bb402226c7e2c465d3beb3c22048d
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-03 23:13:24 -0300

    style: skills component layout refactor

--- portfolio/src/app/components/skills/skills.component.ts
+++ portfolio/src/app/components/skills/skills.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
+import { DropdownComponent } from '../../utils/dropdown/dropdown.component';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [ProgressBarComponent],
+  imports: [ProgressBarComponent, DropdownComponent],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
