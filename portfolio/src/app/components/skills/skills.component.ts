13c434dd7213fc013c0a2c90d7212610df11ce29
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-04 16:11:17 -0300

    feat: Skills component animations

--- portfolio/src/app/components/skills/skills.component.ts
+++ portfolio/src/app/components/skills/skills.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
 import { DropdownComponent } from '../../utils/dropdown/dropdown.component';
+import { floatAnimation, rightFloatAnimation, leftFloatAnimation } from '../../utils/animations';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
   imports: [ProgressBarComponent, DropdownComponent],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.css'
+  styleUrl: './skills.component.css',
+  animations: [floatAnimation, rightFloatAnimation, leftFloatAnimation]
 })
 export class SkillsComponent {
 
